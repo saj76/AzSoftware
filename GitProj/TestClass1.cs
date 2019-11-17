b9eba62724c80fa3e1908b93d70077fd10c0646b
saj76 <saj1397@gmail.com>
2019-11-17 17:30:23 +0330

    add some functions to testClass1

---
+++ GitProj/TestClass1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitProj
+{
+    class TestClass1
+    {
+        public static void TestFunc1(int main) => new NotImplementedException();
+    }
+}
